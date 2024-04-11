6fceb0f9404fc2d91cd9c2c4afdb0eaa0da5a50a
Melissa  Richards <73000073+melissa-richards@users.noreply.github.com>
2024-04-11 13:06:33 -0400

    Add Variables
    First commit of App

---
+++ java-variables/src/JavaVar.java
@@ -0,0 +1,26 @@
+
+public class JavaVar {
+//datatypes
+//class level vars
+
+    public static void main(String[] args) throws Exception {
+        // datatype varName [=value];
+        int games = 3;
+        double allowance = 40.50;
+        long battlePass; //We define battlePass
+
+        battlePass = 15;//We assign a value to battle pass
+
+        // Print out our var values along with a string that explains our values 
+        System.out.println("Number of games I own = " + games);
+        System.out.println("My monthly allowance = " + allowance);
+        System.out.println("Cost of a single battle pass = " + battlePass);
+
+        //With our current Allowance, how many battle passes will we be able to buy?
+
+
+        //What is the total cost of battle passes
+        
+
+    }
+}
